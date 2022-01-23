f8f94ca87ed4965de9366cdb0a9844fff840bdeb
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2022-01-22 22:44:20 -0300

    finalizando sessao projetos

--- Scripts/script.js
+++ Scripts/script.js
@@ -10,3 +10,40 @@ tabsContainer.addEventListener("click", (e)=>{
 		aboutSection.querySelector(target).classList.add("active");
 	}
 });
+
+// portifolio popup
+
+document.addEventListener("click", (e)=>{
+	if(e.target.classList.contains("view-project-btn")){
+		togglePortfolioPopup();
+		portfolioItemDetails(e.target.parentElement);
+	}
+})
+function togglePortfolioPopup(){
+	document.querySelector(".portfolio-popup").classList.toggle("open");
+	document.body.classList.toggle("hide-scrolling");
+	document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click",togglePortfolioPopup);
+
+// esconder popup
+
+document.addEventListener("click", (e)=>{
+	if(e.target.classList.contains("pp-inner")){
+		togglePortfolioPopup();
+		document.querySelector(".portfolio-popup").scrollTo(0,0)
+		portfolioItemDetails(e.target.parentElement)
+	}
+})
+
+function portfolioItemDetails(portfolioItem){
+	document.querySelector(".pp-thumbnail img").src=
+	portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+	
+	document.querySelector(".pp-header h3").innerHTML = 
+	portfolioItem.querySelector(".portfolio-item-title").innerHTML
+
+	document.querySelector(".pp-body").innerHTML = 
+	portfolioItem.querySelector(".portfolio-item-details").innerHTML
+}
